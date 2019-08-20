ea1f046160012e2209aa049c7468b87466644c73
Owen Stranathan <owenstranathan@gmail.com>
2019-08-20 16:25:42 -0400

    fixe some bologna with ycm

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 # some default flags
 # for more information install clang-3.2-doc package and
 # check UsersManual.html
@@ -28,8 +29,32 @@ if sys.platform == "win32":
 # youcompleteme is calling this function to get flags
 # You can also set database for flags. Check: JSONCompilationDatabase.html in
 # clang-3.2-doc package
-def FlagsForFile( filename ):
-  return {
-    'flags': flags,
-    'do_cache': True
-  }
+# def FlagsForFile( filename ):
+#   return {
+#     'flags': flags,
+#     'do_cache': True
+#   }
+
+def Settings(**kwargs):
+    lang = kwargs["language"]
+    ret = { "flags" : flags, "do_cache": True}
+    if lang == "cfamily":
+        filename = kwargs["filename"]
+        if (filename.endswith(".inl")):
+            with open(filename, "r") as inf:
+                line = open(filename, "r").readline()
+                line = line.strip("\n")
+                if line.endswith(".hpp") and line.startswith("//"):
+                    line = line.strip("//")
+                    line = line.replace(" ", "")
+                    dirname = os.path.dirname(filename)
+                    override_name = os.path.join(dirname, line)
+                    if os.path.exists(override_name):
+                        ret["override_filename"] = override_name
+                    return ret
+        else:
+            return ret
+    else:
+        return ret
+
+#  print(Settings(language="cfamily", filename="D:\\code\\wabisoft\\waves\\src\\vector2.inl"))
